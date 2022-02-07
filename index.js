d1a4533895f6e810f31000451ecfef07d9bf0c26
unknown <chanakyachana@gmail.com>
2022-02-08 03:43:12 +0530

    new phones section

--- index.js
+++ index.js
@@ -38,4 +38,26 @@ $(document).ready(function(){
         $grid.isotope({filter:filterValues});
     });
 
+
+    //new phone carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+
+
+
 });
